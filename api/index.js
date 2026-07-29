92ad0870f6ffec015a37c7fc5201036140e51712
ci70153 <ci70153@timeweb.ru>
2026-07-29 16:38:17 +0500

    Fix: also handle auth via GET params

--- api/index.js
+++ api/index.js
@@ -4,16 +4,27 @@ const path = require('path');
 module.exports = async (req, res) => {
   let authData = {};
 
+  // GET params (query string)
+  if (req.query) {
+    for (const key of Object.keys(req.query)) {
+      authData[key] = req.query[key];
+    }
+  }
+
+  // POST body (application/x-www-form-urlencoded)
   if (req.method === 'POST') {
     const body = await new Promise((resolve) => {
       let data = '';
       req.on('data', chunk => data += chunk);
       req.on('end', () => resolve(data));
     });
-
-    const params = new URLSearchParams(body);
-    for (const [key, value] of params) {
-      authData[key] = value;
+    if (body) {
+      try {
+        const params = new URLSearchParams(body);
+        for (const [key, value] of params) {
+          if (!authData[key]) authData[key] = value;
+        }
+      } catch(e) {}
     }
   }
 
@@ -21,7 +32,6 @@ module.exports = async (req, res) => {
   let html = fs.readFileSync(filePath, 'utf8');
 
   const authScript = `<script>window.__BX_AUTH__ = ${JSON.stringify(authData)};</script>`;
-
   html = html.replace('<head>', '<head>' + authScript);
 
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
